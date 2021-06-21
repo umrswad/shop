668208ab93dc31ccb59da88ef21fc7a7afb4f2a1
派大星的脚 <8823779+pai-da-xings-feet@user.noreply.gitee.com>
2021-06-21 19:07:17 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import './plugins/element.js'
 //导入全局样式
 import './assets/css/global.css'
+//导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from "axios";
+//配置请求路径
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login'
+import Home from '../components/home'
 Vue.use(VueRouter)
 const routes = [
   {
@@ -10,9 +11,27 @@ const routes = [
   {
     path:'/login',
     component:Login
+  },
+  {
+    path:'/home',
+    component:Home
   }
 ]
 const router = new VueRouter({
   routes
 })
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next)=>{
+  //to 将要访问的路径
+  //from 代表从哪个路径跳转而来
+  //next 是一个函数，表示方行
+  //   next() 方行     next（'/login'） 强制跳转
+  if (to.path === '/login') return next();
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 export default router
